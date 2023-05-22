297eda12706abc9f9732115a682f98e31fb15dcf
kerdaino <kerdaino3@gmail.com>
2023-05-22 05:21:36 -0700

    keydown-practice2

--- js/script.js
+++ js/script.js
@@ -3,15 +3,32 @@ var modal = document.querySelector(".modal");
 var modalX = document.querySelector(".modal-x");
 
 button.addEventListener("click", function () {
-  modal.classList.add("show-modal");
-  button.innerText = "You've got this!!!";
+  // modal.classList.add("show-modal");
+  // button.innerText = "You've got this!!!";
+  openModal();
 });
 
 modalX.addEventListener("click", function () {
-  modal.classList.remove("show-modal");
-  button.innerText = "Who's got this?";
+  closeModal();
 });
 
 document.addEventListener("keydown", function (e) {
-  console.log(e);
+  // console.log(e.key);
+  if (e.key === "Escape"){
+    // modal.classList.add("show-modal");
+    // button.innerText = "You've got this!!!";
+    if (modal.classList.contains("show-modal")){
+      closeModal();
+    };
+  }
 });
+
+var closeModal = function(){
+  modal.classList.remove("show-modal");
+  button.innerText = "Who's got this?"; 
+}
+
+var openModal = function(){
+  modal.classList.add("show-modal");
+  button.innerText = "You've got this!!!";
+}
